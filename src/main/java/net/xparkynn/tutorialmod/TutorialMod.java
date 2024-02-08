28b53eec302e0af196ff680796e18cd6b8b66037
xParkynn <m.i.schmidt@gmx.de>
2024-02-08 14:03:42 +0100

    Commit

--- src/main/java/net/xparkynn/tutorialmod/TutorialMod.java
+++ src/main/java/net/xparkynn/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.xparkynn.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -11,6 +13,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.xparkynn.tutorialmod.item.ModCreativeModTabs;
+import net.xparkynn.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -19,12 +23,13 @@ public class TutorialMod
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "tutorialmod";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(modEventBus);
+        ModCreativeModTabs.register(modEventBus);
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
---
+++ src/main/java/net/xparkynn/tutorialmod/item/ModCreativeModTabs.java
@@ -0,0 +1,31 @@
+package net.xparkynn.tutorialmod.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.xparkynn.tutorialmod.TutorialMod;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
+    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("tutorial_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
+            .title(Component.translatable("creativetab.tutorial_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.SAPPHIRE.get());
+                        output.accept((ModItems.RAW_SAPPHIRE.get()));
+                        output.accept((ModItems.RED_KEY.get()));
+
+                    })
+
+
+            .build());
+    public static void register(IEventBus eventBus){
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
